e39c30119af2659c0424057300aa6041e8ab470e
Dave Hull <dpgit@trustedsignal.com>
2015-05-07 09:00:44 -0700

    ConvertBase16-ToBase64.ps1
    ConvertBase16-ToBase64.ps1 takes a hexadecimal encoded string and
    returns a base64 encoded string.

---
+++ sets/1/ConvertBase16-ToBase64.ps1
@@ -0,0 +1,24 @@
+﻿<#
+.SYNOPSIS
+hex2base64.ps1 takes a hexadecimal string and returns a base64 encoded
+string.
+#>
+
+
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True,Position=0)]
+        [String]$hexString
+)
+
+$byteString = $(if ( $hexString.Length -eq 1 ) {
+    ([System.Convert]::ToByte( $hexString, 16 ))
+} elseif ( $hexString.Length % 2 -eq 0 ) {
+    $hexString -split "([a-fA-F0-9]{2})" | ForEach-Object {
+        if ($_) {
+            [System.Convert]::ToByte( $_, 16 )
+        }
+    }
+})
+
+[System.Convert]::ToBase64String($byteString)
