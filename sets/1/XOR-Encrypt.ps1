c582dde14b545e6b025a15c945e444678d2ea787
Dave Hull <dpgit@trustedsignal.com>
2015-05-07 21:36:52 -0700

    XOR-Encrypt.ps1
    Removing an unnecessary function.

--- sets/1/XOR-Encrypt.ps1
+++ sets/1/XOR-Encrypt.ps1
@@ -22,24 +22,6 @@ Param(
         [String]$key
 )
 
-function ConvertHex-ToByte {
-Param(
-    [Parameter(Mandatory=$True,Position=0)]
-        [String]$hexString
-)
-    $byteString = $(if ( $hexString.Length -eq 1 ) {
-        ([System.Convert]::ToByte( $hexString, 16 ))
-    } elseif ( $hexString.Length % 2 -eq 0 ) {
-        $hexString -split "([a-fA-F0-9]{2})" | ForEach-Object {
-            if ($_) {
-                [System.Convert]::ToByte( $_, 16 )
-            }
-        }
-    })
-
-    $byteString
-}
-
 function GetBytes {
 Param(
     [Parameter(Mandatory=$True,Position=0)]
