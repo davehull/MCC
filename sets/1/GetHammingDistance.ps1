8ffb8efd4d8c6c80c7237378a49d1a447a8087cf
Dave Hull <dpgit@trustedsignal.com>
2015-05-25 15:35:49 -0700

    GetHammingDistance.ps1
    Added a hashtable to store Hamming Distances in for given byte pairs so
    the calculation only has to be done once per execution. This improves
    run time.

--- sets/1/GetHammingDistance.ps1
+++ sets/1/GetHammingDistance.ps1
@@ -35,6 +35,53 @@ Param(
     [System.Convert]::ToString($byte,2).PadLeft(8,'0') 
 }
 
+
+function GetHammingDistance {
+Param(
+    [Parameter(Mandatory=$True,Position=0)]
+        [byte[]]$ByteArray1,
+    [Parameter(Mandatory=$True,Position=1)]
+        [byte[]]$ByteArray2,
+    [Parameter(Mandatory=$True,Position=2)]
+        [hashtable]$BytePairDist
+)
+    if ($ByteArray1.Count -ne $ByteArray2.Count) {
+        Write-Error ("Hamming Distance can't be calculated because byte arrays are different lengths, {0} and {1}." -f $ByteArray1.Count, $ByteArray2.Count)
+        return $False
+    } else {
+        $Total = 0
+        for ($i = 0; $i -lt $ByteArray1.Count; $i++) {
+            $bitCount = 0
+            # $pair and $rpair are equivalent (10:15 -eq 15:10)
+            $pair  = $(($ByteArray1[$i],$ByteArray2[$i]) -join ":")
+            $rpair = $(($ByteArray2[$i],$ByteArray1[$i]) -join ":")
+            if ($pair -eq $rpair) { 
+                # Write-Verbose ("pair is {0}, Hamming Distance is 0" -f $pair)
+                # Hamming Distance between identical bytes is 0
+                continue
+            } elseif ($BytePairDist.Contains($pair) -or $BytePairDist.Contains($rpair)) {
+                $bitCount += $BytePairDist[$pair]
+                # Write-Verbose ("pair is {0}, Hamming Distance is {1}" -f $pair, $bitCount)
+            } else {
+                $bits = (GetBits ($ByteArray1[$i] -bxor $ByteArray2[$i]))
+
+                for ($j = 0; $j -lt $bits.Length; $j++) {
+                    if ($bits[$j] -eq '1') {
+                        $bitCount++
+                    }
+                }
+                # Write-Verbose ("pair is {0}, Hamming Distance is {1}" -f $pair, $bitCount)
+                $BytePairDist.Add($pair,$bitCount)
+                $BytePairDist.Add($rpair,$bitCount)
+            }
+            $Total += $bitCount
+        }
+        $Total
+    }
+}
+
+
+<#
 function GetHammingDistance {
 Param(
     [Parameter(Mandatory=$True,Position=0)]
@@ -59,8 +106,10 @@ Param(
         $count        
     }
 }
+#>
 
 $byteArray1 = GetBytes $String1
 $byteArray2 = GetBytes $String2
+$BytePairDist = @{}
 
-GetHammingDistance $byteArray1 $byteArray2
+GetHammingDistance $byteArray1 $byteArray2 $BytePairDist
